7db421ec70fc40ffa17c331eda5face5011d6c91
leidada <leidada@qq.com>
2022-11-19 11:38:06 +0800

    加入了js，css

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+  var div = document.querySelector("div");
+  div.addEventListener("click", function () {
+    console.log(1);
+    alert("浩源sb");
+  });
+});
