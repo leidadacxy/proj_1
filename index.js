a41e95ba9c2a114b13501ae5aedb4e8f137f9971
leidada <leidada@qq.com>
2022-11-20 09:59:04 +0800

    移除了某些文件

--- index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function () {
-  var div = document.querySelector("div");
-  div.addEventListener("click", function () {
-    console.log(1);
-    alert("浩源sb");
-  });
-});
